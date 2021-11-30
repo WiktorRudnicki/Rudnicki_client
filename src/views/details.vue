b91316076e148448636e03f14dfa2a3272a5416f
WiktorRudnicki <rudnicki.w01@htlwienwest.at>
2021-11-30 09:15:05 +0100

    breh

--- src/views/details.vue
+++ src/views/details.vue
@@ -1,6 +1,15 @@
 <template>
     <v-app>
         <v-main>
+            <div>
+                <v-img />
+                <h2>{{title}}</h2>
+                <v-datatable>
+
+                </v-datatable>
+                <v-btn></v-btn>
+            </div>
+        </v-main>
     </v-app>
 </template>
 
